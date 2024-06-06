f6cae031926c028ae277ce385e95ad2c8c23450d
Chaffexd <shanechaffe@gmail.com>
2024-06-06 10:01:51 +0100

    added okta auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth"
-import google from "next-auth/providers/google"
+import okta from "next-auth/providers/okta"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [google],
+  providers: [okta],
 })
--- components/SignIn.tsx
+++ components/SignIn.tsx
@@ -2,14 +2,21 @@ import { signIn } from "@/auth";
 
 export function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google");
-      }}
-      className="w-full h-screen flex items-center justify-center"
-    >
-      <button type="submit" className="p-4 bg-cfblue-1  rounded-xl font-bold text-white -mt-44">Signin with Google</button>
-    </form>
+    <>
+      <form
+        action={async () => {
+          "use server";
+          await signIn("okta");
+        }}
+        className="w-full h-screen flex items-center justify-center"
+      >
+        <button
+          type="submit"
+          className="p-4 bg-cfblue-1  rounded-xl font-bold text-white -mt-44 hover:bg-sky-400"
+        >
+          Signin with Okta
+        </button>
+      </form>
+    </>
   );
 }
